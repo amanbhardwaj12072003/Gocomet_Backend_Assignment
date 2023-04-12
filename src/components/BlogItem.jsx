89015b18502dd458d217994edb082e6dcc88bf58
amanbhardwaj12072003 <amanbhardwaj12072003@gmail.com>
2023-04-12 16:06:47 +0530

    API Fetched

--- src/components/BlogItem.jsx
+++ src/components/BlogItem.jsx
@@ -4,11 +4,11 @@ export class BlogItem extends Component {
 
   render() {
     let { title, description, imageUrl, blogUrl } = this.props;
-
+    let defaultImageUrl = "https://mma.prnewswire.com/media/1608177/GoComet_Logo.jpg?p=facebook";
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={imageUrl ? imageUrl : defaultImageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
