91210218936346acd14ee530690c6879def9bc8b
amanbhardwaj12072003 <amanbhardwaj12072003@gmail.com>
2023-04-12 16:38:55 +0530

    Pagination Done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,28 +2,54 @@ import React, { Component } from "react";
 import BlogItem from "./BlogItem";
 
 export class Blog extends Component {
-
   constructor() {
     super();
     console.log("I am constructor");
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
   async componentDidMount() {
     const url =
-      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c333e89ae39a47dfaa2be2fb1888b809";
+      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c333e89ae39a47dfaa2be2fb1888b809&page=1&pageSize=10";
     const data = await fetch(url);
     let parsedData = await data.json();
     // console.log(parsedData);
-    this.setState(
-      {
-        articles : parsedData.articles,
-      }
-    )
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
+
+  handlePrevClick = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c333e89ae39a47dfaa2be2fb1888b809&page=${
+      this.state.page - 1
+    }&pageSize=10`;
+    const data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1,
+    });
+  };
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 10)) {
+    } else {
+      const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c333e89ae39a47dfaa2be2fb1888b809&page=${
+        this.state.page + 1
+      }&pageSize=10`;
+      const data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page + 1,
+      });
+    }
+  };
 
   render() {
     return (
@@ -36,13 +62,34 @@ export class Blog extends Component {
               <div className="col-md-4" key={article.url}>
                 <BlogItem
                   title={article.title ? article.title.slice(0, 45) : " "}
-                  description={article.description ? article.description.slice(0, 88) : " "}
+                  description={
+                    article.description ? article.description.slice(0, 88) : " "
+                  }
                   imageUrl={article.urlToImage}
                   blogUrl={article.url}
                 />
               </div>
             );
           })}
+        </div>
+
+        <div className="d-flex justify-content-evenly">
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+            disabled={this.state.page <= 1}
+          >
+            {" "}
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;{" "}
+          </button>
         </div>
       </div>
     );
